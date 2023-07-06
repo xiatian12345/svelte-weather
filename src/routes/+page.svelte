c64b9c07de32ea0b46f82098bd6e09f454df0fe3
xiatian <2813576925@qq.com>
2023-07-06 19:23:07 +0800

    outline of the app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,42 +3,11 @@
 </script>
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center flex flex-col items-center">
-		<Avatar initials="XT" background="bg-primary-500" />
+<div class="bg-green-100 w-full h-full flex justify-center items-center">
+	<div class="bg-red-100 w-4/5 h-full flex flex-col items-center">
+		<div class='w-60 h-60 bg-cyan-300'></div>
+		<div class='w-60 h-60 bg-yellow-300'></div>
 	</div>
 </div>
-
-<style lang="postcss">
-	figure {
-		@apply flex relative flex-col;
-	}
-	figure svg,
-	.img-bg {
-		@apply w-64 h-64 md:w-80 md:h-80;
-	}
-	.img-bg {
-		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
-			glow 5s linear infinite;
-	}
-	@keyframes glow {
-		0% {
-			@apply bg-primary-400/50;
-		}
-		33% {
-			@apply bg-secondary-400/50;
-		}
-		66% {
-			@apply bg-tertiary-400/50;
-		}
-		100% {
-			@apply bg-primary-400/50;
-		}
-	}
-	@keyframes pulse {
-		50% {
-			transform: scale(1.5);
-		}
-	}
+<style>
 </style>
