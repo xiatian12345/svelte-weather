5e25d6910222c99039cd53e19149ac1b8f5d6d45
xiatian <2813576925@qq.com>
2023-07-06 21:00:25 +0800

    modify the icon of search

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
   import { Search } from "carbon-components-svelte";
+  import SearchAdvanced from "carbon-icons-svelte/lib/SearchAdvanced.svelte";
 </script>
 
-<Search />
+<Search icon={SearchAdvanced}/>
